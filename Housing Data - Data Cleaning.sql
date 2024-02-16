44d1e829fc6c1e965891bf0ac36846f28ff1ce18
Nuella-Mene <74304712+Nuella-Mene@users.noreply.github.com>
2024-02-16 15:03:48 +0100

    Add files via upload

---
+++ Housing Data - Data Cleaning.sql
@@ -0,0 +1,47 @@
+UPDATE [Nashville Housing]
+SET PropertyAddress = NULL
+WHERE PropertyAddress = ''
+
+UPDATE [Nashville Housing]
+SET OwnerName = NULL
+WHERE OwnerName = ''
+
+UPDATE [Nashville Housing]
+SET OwnerAddress = NULL
+WHERE OwnerAddress = ''
+
+UPDATE [Nashville Housing]
+SET Acreage = NULL
+WHERE Acreage = ''
+
+UPDATE [Nashville Housing]
+SET TaxDistrict = NULL
+WHERE TaxDistrict = ''
+
+UPDATE [Nashville Housing]
+SET LandValue = NULL
+WHERE LandValue = ''
+
+UPDATE [Nashville Housing]
+SET BuildingValue = NULL
+WHERE BuildingValue = ''
+
+UPDATE [Nashville Housing]
+SET TotalValue = NULL
+WHERE TotalValue = ''
+
+UPDATE [Nashville Housing]
+SET YearBuilt = NULL
+WHERE YearBuilt = ''
+
+UPDATE [Nashville Housing]
+SET FullBath = NULL
+WHERE FullBath = ''
+
+UPDATE [Nashville Housing]
+SET HalfBath = NULL
+WHERE HalfBath = ''
+
+UPDATE [Nashville Housing]
+SET Bedrooms = NULL
+WHERE Bedrooms = ''
